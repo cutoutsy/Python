17bbc75e00e6a0da5738e92dd516e35a5effa437
fengxiaochuan <fengxiaochuan@corp.netease.com>
2020-04-13 09:51:39 +0800

    知乎模拟登录代码

---
+++ zhihu_login.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 NetEase.com, Inc. All Rights Reserved.
+# Copyright 2020, The Fuxi AI Lab.
+"""
+desc
+
+Authors: fengxiaochuan(fengxiaochuan@corp.netease.com)
+Phone: 15664601006
+Date: 2020/4/12
+
+"""
